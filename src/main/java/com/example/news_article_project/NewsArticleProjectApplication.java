dacd8628ae2866d027708ff47cc4ed39fc51c4a1
이승용 <iseung-yong@iseung-yong-ui-MacBookPro.local>
2025-01-26 17:14:28 +0900

    프로젝트 전체적인 구조 설정

---
+++ src/main/java/com/example/news_article_project/NewsArticleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.news_article_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsArticleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsArticleProjectApplication.class, args);
+	}
+
+}
