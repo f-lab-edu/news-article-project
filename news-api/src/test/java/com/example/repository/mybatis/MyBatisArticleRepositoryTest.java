daa50bd00a2912a9bec35636cebdcc18b5072235
이승용 <iseung-yong@iseung-yong-ui-MacBookPro.local>
2025-02-16 16:42:20 +0900

    Article domain 유닛테스트, 통합테스트 시행 + myBatis 사용하도록 refactoring

--- news-api/src/test/java/com/example/repository/mybatis/MyBatisArticleRepositoryTest.java
+++ news-api/src/test/java/com/example/repository/mybatis/MyBatisArticleRepositoryTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
@@ -20,6 +21,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @Transactional
 @SpringBootTest
+@Sql(scripts = "/sql/initialize_db.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+
 class MyBatisArticleRepositoryTest {
 
     @Autowired
