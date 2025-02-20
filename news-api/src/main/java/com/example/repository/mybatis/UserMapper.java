30be5ed6a36e2be3b4618d799fc4ac87fa86a489
이승용 <iseung-yong@iseung-yong-ui-MacBookPro.local>
2025-02-20 21:18:03 +0900

    user 도메인 repository 계층 구현

---
+++ news-api/src/main/java/com/example/repository/mybatis/UserMapper.java
@@ -0,0 +1,30 @@
+package com.example.repository.mybatis;
+
+import com.example.domain.ArticleCategory;
+import com.example.domain.User;
+import com.example.domain.UserSubscription;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    void insertUser(User user);
+
+    User findById(Long userId);
+
+    void updateUser(User user);
+
+    void deleteUser(Long userId);
+
+    void deleteSubscription(@Param("userId") Long userId, @Param("category") ArticleCategory category, @Param("topic") String topic);
+
+    void insertSubscription(UserSubscription subscription);
+
+    List<UserSubscription> findSubscriptionByUserId(Long userId);
+
+    UserSubscription findOne(@Param("userId") Long userId,
+                             @Param("category") ArticleCategory category,
+                             @Param("topic") String topic);
+}
