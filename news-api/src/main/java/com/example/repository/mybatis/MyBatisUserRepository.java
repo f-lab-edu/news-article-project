847319411ee2a90c9262a662bc3713337e8714c6
이승용 <iseung-yong@iseung-yong-ui-MacBookPro.local>
2025-02-20 21:18:03 +0900

    user 도메인 repository 계층 구현

---
+++ news-api/src/main/java/com/example/repository/mybatis/MyBatisUserRepository.java
@@ -0,0 +1,49 @@
+package com.example.repository.mybatis;
+
+import com.example.domain.ArticleCategory;
+import com.example.domain.User;
+import com.example.domain.UserSubscription;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class MyBatisUserRepository {
+    private final UserMapper userMapper;
+
+    public User save(User user) {
+        userMapper.insertUser(user);
+        return user;
+    }
+
+    public Optional<User> findById(Long userId) {
+        return Optional.ofNullable(userMapper.findById(userId));
+    }
+
+    public void update(User update) {
+        userMapper.updateUser(update);
+    }
+
+    public void delete(Long userId) {
+        userMapper.deleteUser(userId);
+    }
+
+    public List<UserSubscription> findSubscriptions(Long userId) {
+        return userMapper.findSubscriptionByUserId(userId);
+    }
+
+    public void insertSubscription(UserSubscription subscription) {
+        userMapper.insertSubscription(subscription);
+    }
+
+    public void deleteSubscription(Long userId, ArticleCategory category, String topic) {
+        userMapper.deleteSubscription(userId, category, topic);
+    }
+
+    public Optional<UserSubscription> findOneSubscription(Long userId, ArticleCategory category, String topic) {
+        return Optional.ofNullable(userMapper.findOne(userId, category, topic));
+    }
+}
