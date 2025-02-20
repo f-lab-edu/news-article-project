d9cf7e8bfe81dce50368bedfbb1dd6b1eba78c0e
이승용 <iseung-yong@iseung-yong-ui-MacBookPro.local>
2025-02-20 15:25:06 +0900

    UserDTO에서 UserRequestDTO로 이름 변경

--- news-api/src/main/java/com/example/dto/UserRequestDTO.java
+++ news-api/src/main/java/com/example/dto/UserRequestDTO.java
@@ -3,7 +3,7 @@ package com.example.dto;
 import lombok.Data;
 
 @Data
-public class UserDTO {
+public class UserRequestDTO {
     private String email;
     private String username;
     private String password;
